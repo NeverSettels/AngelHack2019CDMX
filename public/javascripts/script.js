43bce0da536febe0993306e38015b7b85287d297
NeverSettels <settels.eric@gmail.com>
2019-07-13 22:18:21 -0500

    el chat ta pedorro pero jala

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    console.log('IronGenerator JS imported successfully!')
+  },
+  false
+)
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+var socket = io()
+$(function() {
+  var socket = io()
+  $('form').submit(function(e) {
+    e.preventDefault() // prevents page reloading
+    socket.emit('chat message', $('#m').val())
+    $('#m').val('')
+    return false
+  })
+  socket.on('chat message', function(msg) {
+    $('#messages').append($('<p  class="rounded-lg bg-light">').text(msg))
+  })
+})
